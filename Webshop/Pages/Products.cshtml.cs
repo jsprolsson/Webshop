ffb858b98f1daedddcf45077a7dc9b5c013914be
Jesper Olsson <jesper.p.olsson@hotmail.com>
2021-11-09 10:40:44 +0100

    working category links

--- Webshop/Pages/Products.cshtml.cs
+++ Webshop/Pages/Products.cshtml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -9,8 +10,21 @@ namespace Webshop.Pages
 {
     public class ProductsModel : PageModel
     {
-        public void OnGet()
+        public List<Models.Product> Products = Data.ProductManager.Products; 
+        public void OnGet(string category)
         {
+            string tempCategory = category;
+
+            if (category == "electronics") {
+                Products = Products.Where(product => product.category == tempCategory).ToList();
+            } else if (category == "jewelery") {
+                Products = Products.Where(product => product.category == tempCategory).ToList();
+            } else if (category == "men's clothing"){
+                Products = Products.Where(product => product.category == tempCategory).ToList();
+            }
         }
+
+        
+
     }
 }
