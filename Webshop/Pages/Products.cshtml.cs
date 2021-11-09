b9f46b5b32d32c00108834846aca0c151df80e57
Jesper Olsson <jesper.p.olsson@hotmail.com>
2021-11-09 13:29:25 +0100

    added search function

--- Webshop/Pages/Products.cshtml.cs
+++ Webshop/Pages/Products.cshtml.cs
@@ -8,23 +8,44 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Webshop.Pages
 {
+    [BindProperties]
     public class ProductsModel : PageModel
     {
-        public List<Models.Product> Products = Data.ProductManager.Products; 
-        public void OnGet(string category)
+        public List<Models.Product> Products = Data.ProductManager.Products;
+        public string SearchMessage { get; set; }
+        public void OnGet(string category, string searchMessage)
         {
-            string tempCategory = category;
+            if (searchMessage != null) Search(searchMessage);
 
-            if (category == "electronics") {
-                Products = Products.Where(product => product.category == tempCategory).ToList();
-            } else if (category == "jewelery") {
-                Products = Products.Where(product => product.category == tempCategory).ToList();
-            } else if (category == "men's clothing"){
-                Products = Products.Where(product => product.category == tempCategory).ToList();
+
+
+            if (category == "electronics")
+            {
+                Products = Products.Where(product => product.category == "electronics").ToList();
+            }
+            else if (category == "jewelery")
+            {
+                Products = Products.Where(product => product.category == "jewelery").ToList();
+            }
+            else if (category == "men's clothing")
+            {
+                Products = Products.Where(product => product.category == "men's clothing").ToList();
+            }
+            else if (category == "women's clothing")
+            {
+                Products = Products.Where(product => product.category == "women's clothing").ToList();
             }
         }
 
-        
+        public void Search(string search)
+        {
+            Products = Products.Where(product => product.category.Contains(search) || product.title.Contains(search) || product.description.Contains(search)).Select(product => product).ToList();
+        }
 
+        public IActionResult OnPostSearch()
+        {
+            return RedirectToPage("/Products", new { SearchMessage });
+        }
+        
     }
 }
