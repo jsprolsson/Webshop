3451700bcce332f9e4aed337a34248167a8a9ef2
Jesper Olsson <jesper.p.olsson@hotmail.com>
2021-11-16 15:36:23 +0100

    changed groupbuy name to group sale

--- Webshop/Pages/Products.cshtml.cs
+++ Webshop/Pages/Products.cshtml.cs
@@ -12,7 +12,7 @@ namespace Webshop.Pages
     public class ProductsModel : PageModel
     {
         public List<Models.Product> Products = Data.ProductManager.Products;
-        public List<Models.GroupBuy> GroupSale = Data.ProductManager.GroupBuyItems;
+        public List<Models.GroupSale> GroupSale = Data.ProductManager.GroupBuyItems;
         public string SearchMessage { get; set; }
 
         [BindProperty(SupportsGet = true)]
